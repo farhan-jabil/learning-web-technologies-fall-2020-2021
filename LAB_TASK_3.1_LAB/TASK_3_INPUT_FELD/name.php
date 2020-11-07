5ddf828594d2487701ce223192c5d4e2ec9b3879
farhan-jabil <farhanhasan295@gmail.com>
2020-11-07 20:35:18 +0600

    Field for name

---
+++ LAB_TASK_3.1_LAB/TASK_3_INPUT_FELD/name.php
@@ -0,0 +1,36 @@
+<?php
+	
+	$data = "";
+
+	if(isset($_REQUEST['submit'])){
+		$name = $_REQUEST['name'];
+		
+		if($name == ""){
+			echo "Fill name";
+		}else{
+			$data = $name;
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NAME</title>
+</head>
+<body>	
+    	
+    <form method="post">
+        
+        <fieldset style="font-family:Calibri;width:200px;height:100px">
+            
+         <legend><b>Name</b></legend>
+         
+         <input type="text" name="name" value="<?=$data?>"> <hr>
+        <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+		
+</body>
+</html>
+
