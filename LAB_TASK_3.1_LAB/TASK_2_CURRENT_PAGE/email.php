489d07130068e1f3401efac8317a9d4afd9067f2
farhan-jabil <farhanhasan295@gmail.com>
2020-11-07 19:33:03 +0600

    CURRENT PAGE

---
+++ LAB_TASK_3.1_LAB/TASK_2_CURRENT_PAGE/email.php
@@ -0,0 +1,40 @@
+
+<html>
+<head>
+    <title>HTML Form -Email</title>
+</head>
+<body>
+    
+    <form method="post" action="email.php">
+        
+        <fieldset style="font-family:Calibri;width:200px;height:100px">
+            
+            <legend><b>EMAIL</b></legend>
+            <input type="email" name="Email">
+            <button title="hint:Sample@example.com" style="color: blue; font-size: 12px;"><b>i</b></button>
+            <hr>
+            <input type="submit" name="submit" value="Submit">
+            
+        </fieldset>
+        
+    </form>
+    
+</body>
+</html>
+
+<?php
+
+if(isset($_REQUEST['submit']))
+{
+    if($_POST['Email']=="")
+    {   
+        echo "No Input";   
+    }
+    else
+    {
+        echo $_POST['Email'];
+    }
+    
+}
+
+?>
