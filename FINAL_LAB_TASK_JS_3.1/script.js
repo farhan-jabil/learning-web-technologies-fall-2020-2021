0394d64c65efac69a672c982bfa248838f3d01d5
farhan-jabil <farhanhasan295@gmail.com>
2020-12-07 09:52:57 +0600

    JS validation

---
+++ FINAL_LAB_TASK_JS_3.1/script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+
+
+let student = ['farhan', '789', 'CS'];
+
+function f1(){
+	let data= document.getElementById('name').value;
+	if(data == ''){
+		document.getElementById('head').innerHTML = 'invalid data';		
+	}else{
+        document.getElementById('head').innerHTML = data;
+        
+    }
+    
+ 
+//emailllll  
+
+
+}
+
+function f2(){
+	let data= document.getElementById('name').value;
+	if(data == ''){
+		document.getElementById('head').innerHTML = 'invalid data';		
+	}else{
+        document.getElementById('head').innerHTML = data;
+        
+    }
+    
+ 
+
+}
